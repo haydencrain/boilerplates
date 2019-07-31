ee21ad1b231fbe9d93e8c202af2c2ccbe308ff05
Hayden Crain <haydencrain@optusnet.com.au>
2019-07-31 22:26:38 +1000

    set up webpack dev server

---
+++ react-typescript/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import duck from '../../images/duck.jpg';
+
+const App = () => (
+	<div>
+		<h1>Hello World!</h1>
+		<img src={duck} height={300} />
+	</div>
+);
+
+export default App;
+
--- react-typescript/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('app'));
-
-// try {
-// 	module.hot.accept();
-// } catch {}
