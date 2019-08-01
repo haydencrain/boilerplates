0048e4cbb8a2a30956d5aa6b6b726fe4ca6d318f
Hayden Crain <haydencrain@optusnet.com.au>
2019-08-01 17:21:56 +1000

    add less support

--- react-typescript/src/components/App/App.jsx
+++ react-typescript/src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import duck from '../../images/duck.jpg';
+import './App.less';
 
 const App = () => (
-	<div>
+	<div className="app">
 		<h1>Hello World!</h1>
-		<img src={duck} height={300} />
+		<img src={duck} />
 	</div>
 );
 
